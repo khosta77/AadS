c09a74cd4dfd4ea4b1be29d46c053d63f4700aff
khosta77 <puwerfulpants@gmail.com>
2024-05-13 03:07:04 +0300

    Задача 3 проходит тесты

--- HW2/task_03/main.cpp
+++ HW2/task_03/main.cpp
@@ -2,7 +2,7 @@
 #include <deque>
 #include <vector>
 #include <fstream>
-#include <queue>
+
 /*
 // Задача 3
 Постройте B-дерево минимального порядка t и выведите его по слоям. В качестве ключа используются числа, 
@@ -27,7 +27,6 @@
 
 //// Код
 
-#if 0
 template <typename T = size_t>  // Взял с семинара
 bool defaultLess( const T &l, const T &r )
 {
@@ -39,383 +38,147 @@ class BTree
 {
     struct Node
     {
-        size_t _order;
         bool _isLeaf;
-    private:
-        bool (*_cmp)( const T&, const T& );
-    public:
-        size_t _size;
+        std::vector<T> _items;
+        std::vector<Node*> _leafs;
 
-        std::vector<T> _keys;
-        Node **_children;
-
-        Node() = delete;
-        Node( const size_t& order, const bool& isLeaf, bool ( *cmp )( const T& l, const T& r ) ) : 
-            _order(order), _isLeaf(isLeaf), _cmp(cmp), _size(0)
-        {
-            _keys.resize( ( ( 2 * _order ) - 1 ) );
-            //_keys = new T[( ( 2 * _order ) - 1 )];
-            _children = new Node*[( 2 * _order )];
-        }
-    
-        ~Node()
-        {
-            for( size_t i = 0; i < _size; ++i )
-                delete[] _children[i];
-            delete[] _children;
-            delete[] _keys;
-        }
-
-        void pushToNode( const T& key )
-        {
-            if( _isLeaf )
-            {
-                T buffer = key;
-                for( size_t i = 0; ( ( i < _size ) ); ++i )
-                    if( _cmp( buffer, _keys.at( i ) ) )
-                        std::swap( buffer, _keys[i] );
-                _keys[_size++] = buffer;
-                return;
-            }
-
-            
-            size_t i = 0;
-            for( ; ( i < _size ); ++i )
-            {
-                if( _cmp( key, _keys.at(i) ) )
-                {
-                //    std::cout << "i: " << i << " Key: " << _keys[i] << std::endl; 
-                    break;
-                }
-            }
-            
-            //size_t i = ( _size - 1 );
-
-            //while( ( ( i >= 0 ) && _cmp( key, _keys[ i ] ) ) )
-              //  --i;
-
-            //std::cout << std::endl;
-            //for(size_t j = 0; j < _size; ++j)
-             //   std::cout << '(' << j << ", " << _keys[j] << ") ";
-            //std::cout << std::endl;
-            //i %= _size;
-            //std::cout << _size << " i:" << i << " Key: " << _keys[i] << " push:" << key << std::endl;
-            
-
-            if( _children[( i )]->_size == ( ( 2 * _order ) - 1 ) )
-            {
-                reBuild( ( i ), _children[( i )] );
-                if( _cmp( _keys[( i )], key ) )
-                    ++i;
-            }
-            _children[( i )]->pushToNode(key);
-        }
-
-        void reBuild( const size_t& I, Node *node )
-        {
-            Node* buffer = new Node( node->_order, node->_isLeaf, _cmp );
-            buffer->_size = ( _order - 1 );
-
-            for( size_t j = 0; j < ( _order - 1 ); ++j )
-                buffer->_keys[j] = node->_keys[j + _order];
-            
-            if( !node->_isLeaf )
-                for( size_t j = 0; j < _order; ++j )
-                    buffer->_children[j] = node->_children[( j + _order )];
-            
-            node->_size = ( _order - 1 );
-            for( size_t j = _size; j >= ( I + 1 ); --j )
-            {
-                //std::cout << j << std::endl;
-                _children[( j + 1 )] = _children[j];
-            }
-            _children[( I + 1 )] = buffer;
-            //for(size_t i = 0; i < _size; ++i)
-            //    std::cout << _keys[i] << ' ';
-            //std::cout << std::endl;
-
-            for( size_t j = _size; j > I; --j )  // Вот тут мог быть выход за границы
-                _keys[( j + 1 )] = _keys[j];
-            _keys[( I + 1 )] = _keys[I];
-
-            _keys[I] = node->_keys[( _order - 1 )];
-            _size = ( _size + 1 );
+        Node( const bool& leaf = false ) : _isLeaf(leaf) {}
+        ~Node() {
+            _items.clear();
+            _leafs.clear();
         }
     };
 
     size_t _order;
     bool (*_cmp)( const T&, const T& );
-    Node *_root;
+    Node* _root;
 
-    /** @brief printNode - выведет ноду и сохранит указатели на ее листья, если есть.
-     * */
-    void printNode( std::deque<Node*>& deq, Node* node )
+    void reBuild( Node* node, const size_t& position )
     {
-        for( size_t i = 0; i < node->_size; ++i)
+        Node* bufferFullNode = node->_leafs[position];
+        Node* bufferEmptyNode = new Node(bufferFullNode->_isLeaf);
+        bufferEmptyNode->_items.resize( ( _order - 1 ) );
+
+        for( size_t j = 0, J = ( _order - 1 ); j < J; ++j )
+            bufferEmptyNode->_items[j] = bufferFullNode->_items[( j + _order )];
+
+        if( !bufferFullNode->_isLeaf )
         {
-            std::cout << node->_keys[i] << ' ';
-            if( !node->_isLeaf )
-                deq.push_back(node->_children[i]);
+            bufferEmptyNode->_leafs.resize(_order);
+            for( size_t j = 0; j < _order; ++j )
+                bufferEmptyNode->_leafs[j] = bufferFullNode->_leafs[( j + _order )];
+            bufferFullNode->_leafs.resize(_order);
         }
-        if( !node->_isLeaf )
-            deq.push_back(node->_children[(node->_size)]);
+
+        node->_leafs.resize( ( node->_leafs.size() + 1 ) );
+        for( size_t j = ( node->_leafs.size() - 1 ), J = ( position + 1 ); j > J; --j )
+            node->_leafs[j] = node->_leafs[( j - 1 )];
+        node->_leafs[( position + 1 )] = bufferEmptyNode;
+
+        node->_items.resize( ( node->_items.size() + 1 ) );
+        for( size_t j = ( node->_items.size() - 1 ); j > position; --j )
+            node->_items[j] = node->_items[( j - 1 )];
+        node->_items[position] = bufferFullNode->_items[( _order - 1 )];
+
+        bufferFullNode->_items.resize( ( _order - 1 ) );
+    }
+
+    void pushToNode( Node* node, const T& item )
+    {
+        int position = ( node->_items.size() - 1 );
+        if( node->_isLeaf )
+        {
+            node->_items.resize( ( node->_items.size() + 1 ) );
+            for( ; ( ( position >= 0 ) && ( _cmp( item, node->_items[position] ) ) ); --position )
+                node->_items[( position + 1 )] = node->_items[position];
+            return ( (void)( node->_items[( position + 1 )] = item ) );
+        }
+
+        for( ; ( ( position >= 0 ) && ( _cmp( item, node->_items[position] ) ) ); --position );
+
+        if( ( node->_leafs[( position + 1 )]->_items.size() == ( ( 2 * _order ) - 1 ) ) )
+        {
+            reBuild( node, ( position + 1 ) );
+            if( _cmp( node->_items[( position + 1 )], item ) )
+                ++position;
+        }
+        pushToNode( node->_leafs[( position + 1 )], item );
     }
 
 public:
-    BTree() = delete;
-    BTree( const size_t& order, bool ( *cmp )( const T& l, const T& r ) = defaultLess ) : _order(order), 
+    BTree( const size_t& order, bool ( *cmp )( const T& l, const T& r ) = defaultLess ) : _order(order),
         _cmp(cmp), _root(nullptr) {}
+    ~BTree()
+    {
+        delete _root;
+    }
 
-    ~BTree() = default;
+    void push( const T& item )
+    {
+        if( _root == nullptr )
+            _root = new Node(true);
+
+        if( ( _root->_items.size() == ( ( 2 * _order ) - 1 ) ) )
+        {
+            Node* node = new Node(false);
+            node->_leafs.push_back(_root);
+            _root = node;
+            reBuild( _root, 0 );
+        }
+
+        pushToNode( _root, item );
+    }
 
     void printLevels()
     {
         if( _root == nullptr )
             return;
 
-        std::deque<Node*> deq;
-        printNode( deq, _root );
-        std::cout << std::endl;
-
-        size_t N = deq.size(), j = 1;
-
-        while( !deq.empty() )
+        std::deque<Node*> nodes;
+        nodes.push_back(_root);
+        while( !nodes.empty() )
         {
-            Node* node = deq.front();
-            deq.pop_front();
-
-            printNode( deq, node );
-            if( (j++) == N )
+            std::deque<Node*> bufferNodes;
+            while( !nodes.empty() )
             {
-                std::cout << std::endl;
-                N = deq.size();
-                j = 0;
-            }       
-        }
-        std::cout << std::endl;
-    }
+                Node* node = nodes.front();
+                nodes.pop_front();
 
-    void push( const T& key )
-    {
-        if ( _root == nullptr )
-        {
-            _root = new Node( _order, true, _cmp );
-            _root->_keys[0] = key;
-            _root->_size = 1;
-            return;
-        }
-        //std::cout << "--------------------------" << std::endl;
-        //std::cout << "Root->Key[0]: " << _root->_keys[0] << " key: " << key << std::endl; 
-        if ( _root->_size != ( ( 2 * _order ) - 1 ) )
-        {
-            //std::cout << "Key: " << key << " added current root" << std::endl;
-            _root->pushToNode(key);
-        //std::cout << _root->_keys[0] << std::endl;
-
-            return; //( (void)( _root->pushToNode(key) ) );
-        }
-        //std::cout << "Key: " << key << " Make new node" << std::endl;
-        Node *node = new Node( _order, false, _cmp );
-        node->_children[0] = _root;
-        node->reBuild( 0, _root );
-        node->_children[( ( _cmp( node->_keys[0], key ) ? 1 : 0 ) )]->pushToNode(key);
-        _root = node;
-
-        //std::cout << _root->_keys[0] << std::endl;
-    }
-};
-#endif
-
-struct Node
-    {
-        size_t _order;
-        bool _isLeaf;
-        size_t _size;
-
-        std::vector<int> _keys;
-        std::vector<Node*> _children;
-
-        Node() = delete;
-        Node( const size_t& order, const bool& isLeaf ) : 
-            _order(order), _isLeaf(isLeaf), _size(0)
-        {
-            _keys.resize( ( ( 2 * _order ) - 1 ) );
-            _children.resize(( 2 * _order ));
-        }
-        ~Node(){}
-};
-
-template<typename T = int>
-class BTree
-{
-public:
-    BTree(size_t order) : _root(nullptr), _order(order) {}
-
-    void push(const T& key)
-    {
-        if (_root == nullptr)
-        {
-            _root = new Node(_order, true);
-            _root->_keys[0] = key;
-            _root->_size = 1;
-        }
-        else
-        {
-            if (_root->_size == (2 * _order - 1))
-            {
-                Node* newRoot = new Node(_order, false);
-                newRoot->_children[0] = _root;
-                splitChild(newRoot, _root, 0);
-                _root = newRoot;
+                for( size_t i = 0; i < node->_items.size(); ++i )
+                    std::cout << node->_items[i] << ' ';
+                for( size_t i = 0; i < node->_leafs.size(); ++i )
+                    bufferNodes.push_back(node->_leafs[i]);
             }
-            insertNonFull(_root, key);
-        }
-    }
-
-    void printByLevel()
-    {
-        if (_root == nullptr)
-        {
-            std::cout << "Tree is emptyn";
-            return;
-        }
-
-        std::queue<Node*> q;
-        q.push(_root);
-
-        while (!q.empty())
-        {
-            int nodeCount = q.size();
-
-            while (nodeCount > 0)
-            {
-                Node* node = q.front();
-                q.pop();
-
-                for (size_t i = 0; i < node->_size; ++i)
-                {
-                    std::cout << node->_keys[i] << " ";
-                }
-
-                if (!node->_isLeaf)
-                {
-                    for (size_t i = 0; i <= node->_size; ++i)
-                    {
-                        q.push(node->_children[i]);
-                    }
-                }
-
-                --nodeCount;
-            }
-
             std::cout << std::endl;
+            nodes = bufferNodes;
         }
     }
-
-private:
-    Node* _root;
-    size_t _order;
-
-    void insertNonFull(Node* node, const T& key)
-    {
-        int i = node->_size - 1;
-
-        if (node->_isLeaf)
-        {
-            while (i >= 0 && key < node->_keys.at(i))
-            {
-                node->_keys.at(i + 1) = node->_keys.at(i);
-                --i;
-            }
-
-            node->_keys.at(i + 1) = key;
-            node->_size++;
-        }
-        else
-        {
-            while (i >= 0 && key < node->_keys.at(i))
-            {
-                --i;
-            }
-
-            ++i;
-
-            if (node->_children.at(i)->_size == 2 * _order - 1)
-            {
-                splitChild(node, node->_children[i], i);
-
-                if (key > node->_keys.at(i))
-                {
-                    ++i;
-                }
-            }
-            insertNonFull(node->_children[i], key);
-        }
-    }
-
-    void splitChild(Node* parent, Node* child, size_t index)
-    {
-        Node* newChild = new Node(_order, child->_isLeaf);
-        newChild->_size = _order - 1;
-
-        for (size_t i = 0; i < _order - 1; ++i)
-        {
-            newChild->_keys[i] = child->_keys[i + _order];
-        }
-
-        if (!child->_isLeaf)
-        {
-            for (size_t i = 0; i < _order; ++i)
-            {
-                newChild->_children[i] = child->_children[i + _order];
-            }
-        }
-
-        child->_size = _order - 1;
-
-        for (int i = parent->_size; i > index; --i)
-        {
-            parent->_children[i + 1] = parent->_children[i];
-        }
-
-        parent->_children[index + 1] = newChild;
-
-        for (int i = parent->_size - 1; i >= index; --i)
-        {
-            parent->_keys[i + 1] = parent->_keys[i];
-        }
-
-        parent->_keys[index] = child->_keys[_order - 1];
-        parent->_size++;
-    }
-
 };
-
 
 //// Тест
 #ifdef MAKETEST
 void test1()
 {
     std::cout << "--------------------------" << std::endl;
-    BTree tree(2);
-    for( size_t i = 0; i < 10; ++i )
+    BTree<int> tree(2);
+    for( int i = 0; i < 10; ++i )
         tree.push(i);
-    tree.printByLevel();
+    tree.printLevels();
 }
 
 void test2()
 {
     std::cout << "--------------------------" << std::endl;
-    BTree tree(4);
-    for( size_t i = 0; i < 10; ++i )
+    BTree<int> tree(4);
+    for( int i = 0; i < 10; ++i )
         tree.push(i);
-    tree.printByLevel();
+    tree.printLevels();
 }
-#if 0
+
 void test3()
 {
     std::cout << "--------------------------" << std::endl;
-    BTree tree(5);
+    BTree<int> tree(5);
     for( size_t i = 9; i > 0; --i )
         tree.push(i);
     tree.push(0);
@@ -425,7 +188,7 @@ void test3()
 void test4()
 {
     std::cout << "--------------------------" << std::endl;
-    BTree tree(2);
+    BTree<int> tree(2);
     tree.push(5);
     tree.push(9);
     tree.push(3);
@@ -469,21 +232,15 @@ private:
 
 void test6()
 {
-    //std::cout << "--------------------------" << std::endl;
     BTree tree(4);
     std::ifstream in("./input/in11.txt", (std::ios::binary | std::ios::in));
     if( !in )
         throw FileNotOpen("input/in11.txt");
-
     size_t key = 0;
-    while( in >> key)
-    {
+    while( in >> key )
         tree.push(key);
-    }
     tree.printLevels();
-
 }
-#endif
 #endif
 
 int main()
@@ -491,9 +248,9 @@ int main()
 #ifdef MAKETEST
     test1();
     test2();
-    //test3();
-    //test4();
-    //test5();
+    test3();
+    test4();
+    test5();
     //test6();
 #else
     size_t order = 0;
@@ -501,9 +258,7 @@ int main()
     BTree tree(order);
     size_t key = 0;
     while( std::cin >> key )
-    {
         tree.push(key);
-    }
     tree.printLevels();
 #endif
     return 0;
