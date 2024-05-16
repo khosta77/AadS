81327c21a63c72d5ab802a10f655ef20a5e7101c
khosta77 <puwerfulpants@gmail.com>
2024-05-16 22:42:14 +0300

    Заготовки под все ДЗ

---
+++ HW3/task_04/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+/*
+// Задача x.x
+*/
+
+// На локальной машине, чтобы поверить
+#define MAKETEST
+#ifdef MAKETEST
+#include <cassert>
+#endif
+
+//// Код
+
+
+
+//// Тест
+#ifdef MAKETEST
+
+#endif
+
+int main()
+{
+#ifdef MAKETEST
+
+#else
+
+#endif
+    return 0;
+}
+
+
