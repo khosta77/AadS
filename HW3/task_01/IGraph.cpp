5094931547454b03c20f4f58e597da3698a6402e
khosta77 <puwerfulpants@gmail.com>
2024-05-22 13:03:48 +0300

    Исправил опечатку

--- HW3/task_01/IGraph.cpp
+++ HW3/task_01/IGraph.cpp
@@ -1,6 +1,6 @@
 #include "IGraph.h"
 
-std::ostream &operator<<( std::ostream &os, const IGraph &graph )
+std::ostream &operator<<( std::ostream &os, const IGraph& graph )
 {
     for( size_t i = 0, I = graph.VerticesCount(); i < I; ++i )
         for( auto j : graph.GetNextVertices(i) )
