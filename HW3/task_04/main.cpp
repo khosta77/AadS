a4eb66f95b944cbadd0447625a7f716988f4857f
khosta77 <puwerfulpants@gmail.com>
2024-05-30 15:22:00 +0300

    Поправил код

--- HW3/task_04/main.cpp
+++ HW3/task_04/main.cpp
@@ -6,6 +6,10 @@
 #include <algorithm>
 #include <cstring>
 #include <valarray>
+#include <exception>
+#include <string>
+#include <cxxabi.h>
+#include <typeinfo>
 
 /*
 // Задача 4.1 Пятнашки
@@ -33,236 +37,299 @@
 
 //// Код
 
-const int SIZE = 16;
-const std::array<int, SIZE> finishField = {1, 2, 3, 4,
-                                                 5, 6, 7, 8,
-                                                 9, 10, 11, 12,
-                                                 13, 14, 15, 0};
+#define SIZE 16
 
-class TagBoard
+const std::array<int, SIZE> masterBoard = {
+    1,  2,  3,  4,
+    5,  6,  7,  8,
+    9,  10, 11, 12,
+    13, 14, 15, 0
+};
+
+class TagException : public std::exception
 {
-    std::array<int, SIZE> field;
-    int emptyPos;
-    int heuristics;
-
-    friend struct GameStateHasher;
-
-    size_t getInvCount() const
-    {
-        size_t inv_count = 0;
-        for( int i = 0; i < SIZE - 1; ++i )
-            for( int j = i + 1; j < SIZE; ++j )
-                if ( ( field[i] > field[j] ) && ( field[i] && field[j] ) )
-                    ++inv_count;
-        return inv_count;
-    }
-
-    int CalcHeuristics()
-    {
-        int res = 0;
-        for( int i = 0; i < SIZE; ++i )
-            res += std::sqrt( ( std::pow( ( ( i % 4 ) - ( ( field[i] - 1 ) % 4 ) ), 2 ) +
-                                std::pow( ( ( i / 4 ) - ( ( field[i] - 1 ) / 4 ) ), 2 ) ) );
-        heuristics = res;
-        return heuristics;
-    }
+private:
+    std::string m_msg;
 
 public:
-    TagBoard(const std::array<int, SIZE> &field) : field(field), emptyPos(-1), heuristics(-1)
+    explicit TagException(const std::string &msg) : m_msg(msg) {}
+    const char *what() const noexcept override
     {
-        for (int i = 0; i < SIZE; i++)
-            if (field[i] == 0)
-                emptyPos = i;
-        assert(emptyPos != -1);
-        CalcHeuristics();
-    }
-
-    bool IsComplete() const { return field == finishField; }
-    bool IsSolvable() const { return (getInvCount() + ( (emptyPos / 4) + 1) ) % 2 == 0; }
-    bool CanMoveLeft() const { return emptyPos % 4 != 3; }
-    bool CanMoveRight() const { return emptyPos % 4 != 0; }
-    bool CanMoveUp() const { return emptyPos < 12; }
-    bool CanMoveDown() const { return emptyPos > 3; }
-
-    TagBoard MoveLeft()
-    {
-        assert(CanMoveLeft());
-        TagBoard newState(*this);
-        std::swap( newState.field[emptyPos], newState.field[( emptyPos + 1 )] );
-        ++newState.emptyPos;
-        CalcHeuristics();
-        return newState;
-    }
-
-    TagBoard MoveRight()
-    {
-        assert(CanMoveRight());
-        TagBoard newState(*this);
-        std::swap( newState.field[emptyPos], newState.field[( emptyPos - 1 )] );
-        --newState.emptyPos;
-        CalcHeuristics();
-        return newState;
-    }
-
-    TagBoard MoveUp()
-    {
-        assert(CanMoveUp());
-        TagBoard newState(*this);
-        std::swap( newState.field[emptyPos], newState.field[( emptyPos + 4 )] );
-        newState.emptyPos += 4;
-        CalcHeuristics();
-        return newState;
-    }
-
-    TagBoard MoveDown()
-    {
-        assert(CanMoveDown());
-        TagBoard newState(*this);
-        std::swap( newState.field[emptyPos], newState.field[( emptyPos - 4 )] );
-        newState.emptyPos -= 4;
-        CalcHeuristics();
-        return newState;
-    }
-
-    bool operator==(const TagBoard &other) const { return field == other.field; }
-    bool operator>( const TagBoard &rhs ) const { return ( heuristics > rhs.heuristics ); }
-
-    const int getHeuristics() const { return heuristics; }
-
+		return m_msg.c_str();
+	}
 };
 
-struct GameStateHasher
+class PositionEquallyMinusOne : public TagException
 {
-    size_t operator()(const TagBoard &state) const
-    {
-        size_t hash = 0;
-        std::memcpy(&hash, state.field.data(), sizeof(hash));
-        return hash;
-    }
+public:
+    explicit PositionEquallyMinusOne() : TagException(
+            "Данная пятнашка не решаемая"
+    ) {}
 };
 
-struct StateComp
+class MoveThrow : public TagException
 {
-    constexpr bool operator()( TagBoard const &a, TagBoard const &b )
-    const noexcept { return a > b; }
+public:
+    explicit MoveThrow( const std::string &move ) : TagException(
+            "В ходе перемещения " + move + " что-то пошло не так"
+    ) {}
 };
 
 class Game
 {
-
-public:
-    Game( const std::array<int, SIZE> &field )
-};
-
-std::pair<int, std::string> GetSolution( const std::array<int, SIZE> &field )
-{
-    TagBoard startState(field);
-    if (!startState.IsSolvable())
-        return std::make_pair(-1, "");
-
-    std::unordered_map<TagBoard, int, GameStateHasher> visited;
-    visited[startState] = 'S';
-
-    std::priority_queue<TagBoard, std::vector<TagBoard>, StateComp> queue;
-
-    queue.push(startState);
-
-    while( !queue.empty() )
+    struct TagBoard
     {
-        TagBoard state = queue.top();
-        queue.pop();
+        std::array<int, SIZE> _board;
+        int _emptyPos;
+        int _heuristics;
 
-        if (state.IsComplete())
-            break;
-
-        if( state.CanMoveLeft() )
+        size_t getInvCount() const
         {
-            TagBoard newState = state.MoveLeft();
-            if( visited.find(newState) == visited.end() )
-            {
-                visited[newState] = 'L';
-                queue.push(newState);
-            }
+            size_t inv_count = 0;
+            for( int i = 0; i < SIZE - 1; ++i )
+                for( int j = i + 1; j < SIZE; ++j )
+                   if ( ( _board[i] > _board[j] ) && ( _board[i] && _board[j] ) )
+                        ++inv_count;
+            return inv_count;
         }
 
-        if( state.CanMoveRight() )
+        int CalcHeuristics()
         {
-            TagBoard newState = state.MoveRight();
-            if (visited.find(newState) == visited.end())
-            {
-                visited[newState] = 'R';
-                queue.push(newState);
-            }
+            _heuristics = 0;
+            for( int i = 0; i < SIZE; ++i )
+                _heuristics += std::sqrt( ( std::pow( ( ( i % 4 ) - ( ( _board[i] - 1 ) % 4 ) ), 2 ) +
+                                            std::pow( ( ( i / 4 ) - ( ( _board[i] - 1 ) / 4 ) ), 2 ) ) );
+            return _heuristics;
         }
 
-        if( state.CanMoveUp() )
+        TagBoard(const std::array<int, SIZE> &board) : _board(board), _emptyPos(-1), _heuristics(-1)
         {
-            TagBoard newState = state.MoveUp();
-            if (visited.find(newState) == visited.end())
-            {
-                visited[newState] = 'U';
-                queue.push(newState);
-            }
+            for (int i = 0; i < SIZE; i++)
+                if (_board[i] == 0)
+                    _emptyPos = i;
+            if( _emptyPos == -1 )
+                throw PositionEquallyMinusOne();
+            CalcHeuristics();
         }
-        if( state.CanMoveDown() )
+
+        bool IsComplete( std::array<int, SIZE> &check ) const { return ( _board == check ); }
+        bool IsSolvable() const { return ( ( ( getInvCount() + ( (_emptyPos / 4 ) + 1 ) ) % 2 ) == 0 ); }
+        bool CanMoveLeft() const { return ( ( _emptyPos % 4 ) != 3 ); }
+        bool CanMoveRight() const { return ( ( _emptyPos % 4 ) != 0 ); }
+        bool CanMoveUp() const { return ( _emptyPos < 12 ); }
+        bool CanMoveDown() const { return ( _emptyPos > 3 ); }
+
+        TagBoard MoveLeft()
         {
-            TagBoard newState = state.MoveDown();
-            if( visited.find(newState) == visited.end() )
-            {
-                visited[newState] = 'D';
-                queue.push(newState);
-            }
+            if( !CanMoveLeft() )
+                throw MoveThrow("Left");
+            TagBoard newState(*this);
+            std::swap( newState._board[_emptyPos], newState._board[( _emptyPos + 1 )] );
+            ++newState._emptyPos;
+            CalcHeuristics();
+            return newState;
+        }
+
+        TagBoard MoveRight()
+        {
+            if( !CanMoveRight() )
+                throw MoveThrow("Right");
+            TagBoard newState(*this);
+            std::swap( newState._board[_emptyPos], newState._board[( _emptyPos - 1 )] );
+            --newState._emptyPos;
+            CalcHeuristics();
+            return newState;
+        }
+
+        TagBoard MoveUp()
+        {
+            if( !CanMoveUp() )
+                throw MoveThrow("Up");
+            TagBoard newState(*this);
+            std::swap( newState._board[_emptyPos], newState._board[( _emptyPos + 4 )] );
+            newState._emptyPos += 4;
+            CalcHeuristics();
+            return newState;
+        }
+
+        TagBoard MoveDown()
+        {
+            if( !CanMoveDown() )
+                throw MoveThrow("Down");
+            TagBoard newState(*this);
+            std::swap( newState._board[_emptyPos], newState._board[( _emptyPos - 4 )] );
+            newState._emptyPos -= 4;
+            CalcHeuristics();
+            return newState;
+        }
+
+        bool operator==( const TagBoard &other ) const { return ( _board == other._board ); }
+        bool operator>( const TagBoard &rhs ) const { return ( _heuristics > rhs._heuristics ); }
+    };
+
+    struct GameStateHasher
+    {
+        size_t operator()( const TagBoard &state ) const
+        {
+            size_t hash = 0;
+            std::memcpy( &hash, state._board.data(), sizeof(hash) );
+            return hash;
+        }
+    };
+
+    struct StateComp
+    {
+        constexpr bool operator()( const TagBoard &lhs, const TagBoard &rhs )
+        const noexcept { return lhs > rhs; }
+    };
+
+    std::array<int, SIZE> _controlBoard;
+    std::array<int, SIZE> _board;
+    TagBoard _startState;
+    std::unordered_map<TagBoard, int, GameStateHasher> _visited;
+    std::priority_queue<TagBoard, std::vector<TagBoard>, StateComp> _queue;
+    std::string _path;
+    int _counter;
+
+    void twistLeft( TagBoard &state )
+    {
+        TagBoard newState = state.MoveLeft();
+        if( _visited.find(newState) == _visited.end() )
+        {
+            _visited[newState] = 'L';
+            _queue.push(newState);
+        }    
+    }
+    
+    void twistRight( TagBoard &state )
+    {
+        TagBoard newState = state.MoveRight();
+        if (_visited.find(newState) == _visited.end())
+        {
+            _visited[newState] = 'R';
+            _queue.push(newState);
+        }    
+    }
+    
+    void twistUp( TagBoard &state )
+    {
+        TagBoard newState = state.MoveUp();
+        if (_visited.find(newState) == _visited.end())
+        {
+            _visited[newState] = 'U';
+            _queue.push(newState);
+        }      
+    }
+    
+    void twistDown( TagBoard &state )
+    {
+        TagBoard newState = state.MoveDown();
+        if( _visited.find(newState) == _visited.end() )
+        {
+            _visited[newState] = 'D';
+            _queue.push(newState);
+        }     
+    }
+
+    void twist()
+    {
+        while( !_queue.empty() )
+        {
+            TagBoard state = _queue.top();
+            _queue.pop();
+
+            if (state.IsComplete(_controlBoard))
+                break;
+
+            if( state.CanMoveLeft() )
+                twistLeft(state);
+
+            if( state.CanMoveRight() )
+                twistRight(state);
+ 
+            if( state.CanMoveUp() )
+                twistUp(state);
+            
+            if( state.CanMoveDown() )
+                twistDown(state);
         }
     }
 
-    std::string path;
-    GameState state(finishField);
-    int counter = 0;
-    while( visited[state] != 'S' )
+    void switchPath( TagBoard &state, const int &move )
     {
-        char move = visited[state];
-        counter++;
         switch( move )
         {
             case 'L':
             {
                 state = state.MoveRight();
-                path += 'L';
+                _path += 'L';
                 break;
             }
             case 'R':
             {
                 state = state.MoveLeft();
-                path += 'R';
+                _path += 'R';
                 break;
             }
             case 'D':
             {
                 state = state.MoveUp();
-                path += 'D';
+                _path += 'D';
                 break;
             }
             case 'U':
             {
                 state = state.MoveDown();
-                path += 'U';
+                _path += 'U';
                 break;
             }
         }
     }
-    std::reverse(path.begin(), path.end());
-    return std::pair<int, std::string>( counter, path );
+
+    void findPath()
+    {
+        TagBoard state(_controlBoard);
+        while( _visited[state] != 'S' )
+        {
+            ++_counter;
+            switchPath( state, _visited[state] );
+        }
+        std::reverse( _path.begin(), _path.end() );
+    }
+
+public:
+    Game( const std::array<int, SIZE> &field, const std::array<int, SIZE> &controlBoard = masterBoard ) : 
+        _controlBoard(controlBoard), _board(field), _startState(_board), _counter(0) {}
+    
+    std::pair<int, std::string> play()
+    {
+        if (!_startState.IsSolvable())
+            return std::make_pair(-1, "");
+        _visited[_startState] = 'S';
+        _queue.push(_startState);
+        twist();
+        findPath();
+        return std::pair<int, std::string>( _counter, _path );
+    }
+};
+
+std::array<int, SIZE> readArray( std::istream &in )
+{
+    std::array<int, SIZE> array;
+    for (int i = 0, elem = 0; ( ( i < SIZE ) && ( in >> elem ) ); ++i)
+        array[i] = elem;
+    return array;
 }
 
-std::pair<int, std::string> run(std::istream &in, std::ostream &out)
+void printPair( const std::pair<int, std::string> &pair, std::ostream &out )
 {
-    std::array<int, SIZE> field;
-    for (int i = 0; i < SIZE; ++i) {
-        int buf = 0;
-        in >> buf;
-        field[i] = buf;
-    }
-    return GetSolution(field);
+    out << pair.first << '\n' << pair.second << std::endl;   
+}
+
+void run( std::istream &in, std::ostream &out )
+{
+    Game game( readArray( in ) );
+    printPair( game.play(), out );
 }
 
 //// Тест
@@ -271,7 +338,8 @@ const std::array<int, SIZE> TEST_01 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 13
 
 void makeTest( const std::array<int, SIZE> firstState, const std::pair<int, std::string> resultTrue )
 {
-    auto result = GetSolution(firstState);
+    Game game(firstState);
+    auto result = game.play();
     assert( result.first == resultTrue.first );
     assert( result.second == resultTrue.second );
 }
@@ -282,7 +350,7 @@ int main()
 #ifdef MAKETEST
     makeTest( TEST_01, std::pair<int, std::string>( 1, "U" ) );
 #else
-    run(std::cin, std::cout);
+    run( std::cin, std::cout );
 #endif
     return 0;
 }
